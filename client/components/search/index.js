074fa56c6de0e9d4abd6be99a854999d77d665fb
Joanna Kao <joanna.kao@ft.com>
2017-03-29 14:11:34 -0400

    load data into dropdowns. tie industries + jobs (major+minor groups)

--- client/components/search/index.js
+++ client/components/search/index.js
@@ -1,22 +1,65 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import * as d3 from 'd3';
 
 class Search extends Component {
-  render() {
-    const dropdownIndustry = (<select>
-      <option>Industry 1</option>
-    </select>);
+  constructor(props) {
+    super(props);
 
-    const dropdownOccupation = (<select>
-      <option>Occupation 1</option>
-    </select>);
+    this.state = {
+      industries: this.props.industries,
+      jobs: [],
+    };
+
+    this.getJobsList = this.getJobsList.bind(this);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({
+      industries: nextProps.industries,
+    });
+  }
+
+  getJobsList(majorGroupId) {
+    axios
+      .get('./data/minor_groups.csv')
+      .then((response) => {
+        const allJobs = d3.csvParse(response.data);
+        const filteredJobs = allJobs.filter((job) => job.id.split('-')[0] === majorGroupId);
+
+        this.setState({
+          jobs: filteredJobs,
+        });
+      });
+  }
+
+  render() {
+    const dropdownIndustry = this.state.industries.map((industry) => {
+      return (<option value={industry.id.split('-')[0]}>{industry.major_group_title}</option>)
+    });
+
+    const dropdownOccupation = this.state.jobs.map((industry) => {
+      return (<option>{industry.minor_group_title}</option>)
+    });
 
     return (
       <div id="search-container">
-        <input type="text" placeholder="Enter your occupation here..." />
-
+        <div id="preset-button-container">
+          <span>Popular occupations: </span>
+          <button className="o-buttons">Lawyer</button>
+          <button className="o-buttons">Investment Banker</button>
+          <button className="o-buttons">Clergy</button>
+          <button className="o-buttons">Show me a random occupation</button>
+        </div>
         <div id="category-container">
-          {dropdownIndustry}
-          {dropdownOccupation}
+          <select onChange={event => this.getJobsList(event.target.value)}>
+            <option>Pick an industry</option>
+            {dropdownIndustry}
+          </select>
+          <select disabled={dropdownOccupation.length <= 0}>
+            <option>Pick a job</option>
+            {dropdownOccupation}
+          </select>
         </div>
       </div>
     );
@@ -24,7 +67,7 @@ class Search extends Component {
 }
 
 Search.propTypes = {
-  data: React.PropTypes.array,
+  industries: React.PropTypes.array,
 };
 
 export default Search;
--- client/index.js
+++ client/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
+import * as d3 from 'd3';
 
 import Search from './components/search';
 
@@ -9,20 +11,33 @@ class App extends Component {
 
     this.state = {
       data: [],
+      industries: [],
+      jobs: [],
     };
+  }
+
+  componentDidMount() {
+    axios
+      .get('./data/data_sample.csv')
+      .then((response) => {
+        this.setState({
+          data: d3.csvParse(response.data),
+        });
+      });
+
+    axios
+      .get('./data/major_groups.csv')
+      .then((response) => {
+        this.setState({
+          industries: d3.csvParse(response.data),
+        });
+      });
   }
 
   render() {
     return (
       <div>
-        <div id="preset-button-container">
-          <span>Popular occupations: </span>
-          <button className="o-buttons">Lawyer</button>
-          <button className="o-buttons">Investment Banker</button>
-          <button className="o-buttons">Clergy</button>
-          <button className="o-buttons">Show me a random occupation</button>
-        </div>
-        <Search data={this.state.data} />
+        <Search industries={this.state.industries} />
       </div>
     );
   }
