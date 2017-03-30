48017c7947ecca3c907e278ae2134f626ae384e4
Joanna Kao <joanna.kao@ft.com>
2017-03-30 14:37:17 -0400

    add structure to activities container

--- client/components/activities/index.js
+++ client/components/activities/index.js
@@ -15,9 +15,19 @@ class Activities extends Component {
   }
 
   render() {
+    const activities = () => {
+      return (<fieldset className="o-forms">
+        <input type="checkbox" name="checkbox51" value="1" className="o-forms__checkbox" id="checkbox51" checked />
+        <label htmlFor="checkbox51" className="o-forms__label"><p>I confirm I have read and agree to the terms &amp; conditions, privacy policy and cookie policy*.</p></label>
+      </fieldset>);
+    };
+
     return (
       <div id="activities-container">
+        <div className="step-number o-forms__prefix">3</div>
         <h2 className="o-typography-heading2">How much of my job could a robot do?</h2>
+        <p id="activities-container__instructions">Select all activities that apply.</p>
+        {activities()}
       </div>
     );
   }
