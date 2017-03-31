7047ef9b17b1e9bfcb276575803d8c250abfdcc6
Joanna Kao <joanna.kao@ft.com>
2017-03-31 12:40:50 -0400

    switch order of no and sometimes in bars

--- client/components/proportional-stacked-bar/index.js
+++ client/components/proportional-stacked-bar/index.js
@@ -8,22 +8,22 @@ class ProportionalStackedBarChart extends Component {
         width: `${((d.data.yes * 100) / d.data.numJobActivities)}%`,
       };
 
-      const noStyles = {
+      const sometimesStyles = {
         left: `${((d.data.yes * 100) / d.data.numJobActivities)}%`,
-        width: `${((d.data.no * 100) / d.data.numJobActivities)}%`,
+        width: `${((d.data.sometimes * 100) / d.data.numJobActivities)}%`,
       };
 
-      const sometimesStyles = {
-        left: `${(((d.data.yes + d.data.no) * 100) / d.data.numJobActivities)}%`,
-        width: `${((d.data.sometimes * 100) / d.data.numJobActivities)}%`,
+      const noStyles = {
+        left: `${(((d.data.yes + d.data.sometimes) * 100) / d.data.numJobActivities)}%`,
+        width: `${((d.data.no * 100) / d.data.numJobActivities)}%`,
       };
 
       return (<div className="bar-container">
         <label htmlFor={`bar__${d.key}`}>{d.label}:</label>
         <div id={`bar__${d.key}`} className="bar">
           <div id={`bar__${d.key}-yes`} className="bar-segment bar-yes" style={yesStyles} />
-          <div id={`bar__${d.key}-no`} className="bar-segment bar-no" style={noStyles} />
           <div id={`bar__${d.key}-sometimes`} className="bar-segment bar-sometimes" style={sometimesStyles} />
+          <div id={`bar__${d.key}-no`} className="bar-segment bar-no" style={noStyles} />
         </div>
       </div>);
     });
